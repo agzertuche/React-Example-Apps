b5708bf1db033f3a58dd0c71aab3259fe1a7d1cb
arturo de la garza zertuche <agzertuche@gmail.com>
2017-04-23 00:19:32 -0500

    Weather app

---
+++ ReactWeather/app/app.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('main');
+var Weather = require('weather');
+var About = require('about');
+var Examples = require('examples');
+
+ReactDOM.render(
+    <Router history={hashHistory}>
+            <Route path="/" component={Main}>
+            <Route path="about" component={About}/>
+            <Route path="examples" component={Examples}/>
+            <IndexRoute component={Weather}/>
+            </Route>
+    </Router>,
+    document.getElementById('app')
+);
---
+++ ReactWeather/app/components/about.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+// Arrow function for presentation components *statless*
+
+var About = (props) => {
+    return (
+        <h3>About Component</h3>
+    )
+};
+
+module.exports = About;
